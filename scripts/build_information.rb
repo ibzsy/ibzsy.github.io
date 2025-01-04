03ec18550a31b83fbdc49780e935d27672b5d7d5
ibzsy <ibzsy@proton.me>
2025-01-04 15:56:31 +0000

    book overhaul

--- scripts/build_information.rb
+++ scripts/build_information.rb
@@ -1,11 +1,13 @@
 require 'yaml'
 
-file_path = 'data/site_version.yml'
+file_path = 'data/build_information.yml'
+
 version = `git rev-parse HEAD`.strip[0, 7]
+date = `git show --no-patch --no-notes --pretty='%cd'`.strip.downcase
 
 begin
   File.open(file_path, 'w') do |file|
-    file.write({ 'version' => version }.to_yaml)
+    file.write({ 'build' => version, 'date' => date }.to_yaml)
   end
 rescue StandardError => e
   puts "Failed to write to file: #{e.message}"
--- scripts/goodreads_rss.rb
+++ scripts/goodreads_rss.rb
@@ -1,19 +1,51 @@
-require 'yaml'
 require 'rss'
 require 'open-uri'
+require 'yaml'
 
 books_read_url = 'https://www.goodreads.com/review/list_rss/153416087?shelf=read'
+currently_reading_url = 'https://www.goodreads.com/review/list_rss/153416087?shelf=currently-reading'
+
 file_path = 'data/goodreads_books.yml'
 
+currently_reading_book = []
 read_books = []
 
+author_regex = %r{author:\s*(.*?)<br/>}
+
+# Fetch and parse the RSS feed for read books
 URI.parse(books_read_url).open do |rss|
   feed = RSS::Parser.parse(rss)
-  feed.items.each do |item|
-    read_books << { 'title' => item.title.downcase, 'link' => item.link }
+  items = feed.items.sort_by { |item| Time.parse(item.pubDate.to_s) }.reverse
+  items.first(3).each do |item|
+    author_match = item.description.match(author_regex)
+    author = author_match ? author_match[1] : 'unknown'
+    read_books << {
+      'title' => item.title.downcase,
+      'link' => item.link,
+      'author' => author.downcase
+    }
   end
 end
 
-File.open(file_path, 'w') do |file|
-  file.write({ 'read_books' => read_books }.to_yaml)
+# Fetch and parse the RSS feed for currently reading book
+URI.parse(currently_reading_url).open do |rss|
+  feed = RSS::Parser.parse(rss)
+  item = feed.items.first
+  author_match = item.description.match(author_regex)
+  author = author_match ? author_match[1] : 'unknown'
+  currently_reading_book << {
+    'title' => item.title.downcase,
+    'link' => item.link,
+    'author' => author.downcase
+  }
 end
+
+# Write the data to the YAML file
+File.open(file_path, 'w') do |file|
+  file.write({
+    'read_books' => read_books,
+    'current_book' => currently_reading_book
+  }.to_yaml)
+end
+
+puts "Book information written to #{file_path}"
