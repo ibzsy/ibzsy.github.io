0b3c1f2d576acefdceac54fd984206e7bfa9dd0e
ibzsy <ibzsy@proton.me>
2025-01-05 13:56:50 +0000

    fix date

--- scripts/build_information.rb
+++ scripts/build_information.rb
@@ -1,9 +1,10 @@
 require 'yaml'
+require 'date'
 
 file_path = 'data/build_information.yml'
 
 version = `git rev-parse HEAD`.strip[0, 7]
-date = `git show --no-patch --no-notes --pretty='%cd'`.strip.downcase
+date = DateTime.now.strftime '%d/%m/%Y %H:%M'
 
 begin
   File.open(file_path, 'w') do |file|
